5694b857e6c414dff5e25dee44929faf780ccc13
Aliq <Alexandr.V.Kim@kaspi.kz>
2019-05-13 19:49:23 +0600

    доавбил свой класс-создатель

---
+++ Asp.net MVC .Net Framework/WebApplication1/App_Start/MyDependencyResolver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Unity;
+using Unity.AspNet.Mvc;
+
+namespace WebApplication1.App_Start
+{
+    public class MyDependencyResolver : UnityDependencyResolver, IDependencyResolver
+    {
+        public MyDependencyResolver(IUnityContainer container) : base(container)
+        { }
+
+
+        object IDependencyResolver.GetService(Type serviceType)
+        {
+            return base.GetService(serviceType);
+        }
+    }
+}
