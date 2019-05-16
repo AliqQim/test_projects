c70762dfd64e35bfbdeee386a19f8d34e36d67f0
Aliq <Alexandr.V.Kim@kaspi.kz>
2019-05-16 19:48:09 +0600

    поставил Unity 3.5 вместо свежей

--- Asp.net MVC .Net Framework/WebApplication1/App_Start/UnityMvcActivator.cs
+++ Asp.net MVC .Net Framework/WebApplication1/App_Start/UnityMvcActivator.cs
@@ -1,39 +1,34 @@
 using System.Linq;
 using System.Web.Mvc;
+using Microsoft.Practices.Unity.Mvc;
 
-using Unity.AspNet.Mvc;
-using WebApplication1.App_Start;
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(WebApplication1.App_Start.UnityWebActivator), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethod(typeof(WebApplication1.App_Start.UnityWebActivator), "Shutdown")]
 
-[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(WebApplication1.UnityMvcActivator), nameof(WebApplication1.UnityMvcActivator.Start))]
-[assembly: WebActivatorEx.ApplicationShutdownMethod(typeof(WebApplication1.UnityMvcActivator), nameof(WebApplication1.UnityMvcActivator.Shutdown))]
-
-namespace WebApplication1
+namespace WebApplication1.App_Start
 {
-    /// <summary>
-    /// Provides the bootstrapping for integrating Unity with ASP.NET MVC.
-    /// </summary>
-    public static class UnityMvcActivator
+    /// <summary>Provides the bootstrapping for integrating Unity with ASP.NET MVC.</summary>
+    public static class UnityWebActivator
     {
-        /// <summary>
-        /// Integrates Unity when the application starts.
-        /// </summary>
+        /// <summary>Integrates Unity when the application starts.</summary>
         public static void Start() 
         {
-            FilterProviders.Providers.Remove(FilterProviders.Providers.OfType<FilterAttributeFilterProvider>().First());
-            FilterProviders.Providers.Add(new UnityFilterAttributeFilterProvider(UnityConfig.Container));
+            var container = UnityConfig.GetConfiguredContainer();
 
-            DependencyResolver.SetResolver(new MyDependencyResolver(UnityConfig.Container));
+            FilterProviders.Providers.Remove(FilterProviders.Providers.OfType<FilterAttributeFilterProvider>().First());
+            FilterProviders.Providers.Add(new UnityFilterAttributeFilterProvider(container));
+
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
 
             // TODO: Uncomment if you want to use PerRequestLifetimeManager
             // Microsoft.Web.Infrastructure.DynamicModuleHelper.DynamicModuleUtility.RegisterModule(typeof(UnityPerRequestHttpModule));
         }
 
-        /// <summary>
-        /// Disposes the Unity container when the application is shut down.
-        /// </summary>
+        /// <summary>Disposes the Unity container when the application is shut down.</summary>
         public static void Shutdown()
         {
-            UnityConfig.Container.Dispose();
+            var container = UnityConfig.GetConfiguredContainer();
+            container.Dispose();
         }
     }
 }
