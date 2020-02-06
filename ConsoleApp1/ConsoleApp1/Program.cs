b7711b4a72e19217052920a0065b82827b5e5116
Aliq <Alexandr.V.Kim@kaspi.kz>
2020-02-06 16:52:24 +0600

    готово

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,12 +8,25 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+
+
+    //https://support.smartbear.com/alertsite/docs/monitors/api/endpoint/jsonpath.html
+
     class Program
     {
         static void Main(string[] args)
         {
             
             JObject o = JObject.Parse(File.ReadAllText("data.json"));
+            var indices = (JObject) o.SelectToken("indices");
+            foreach (var index in indices)
+            {
+                string title = index.Key;
+
+                int size = (index.Value as JObject).SelectTokens("..size_in_bytes").First().Value<int>();
+
+                Console.WriteLine($"{title}\t\t\t{ size }");
+            }
 
 
             Console.WriteLine("DONE");
