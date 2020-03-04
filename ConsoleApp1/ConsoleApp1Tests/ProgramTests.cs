4483e54f1ea55931613468aebcf184a721e85357
Aliq <Alexandr.V.Kim@kaspi.kz>
2020-03-04 13:39:12 +0600

    местный компаратор работает

--- ConsoleApp1/ConsoleApp1Tests/ProgramTests.cs
+++ ConsoleApp1/ConsoleApp1Tests/ProgramTests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace ConsoleApp1.Tests
 {
@@ -15,13 +16,32 @@ namespace ConsoleApp1.Tests
         public int i;
     }
 
+    public class MyComparer : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            return ((A)x).i - ((A)y).i;
+        }
+    }
+
     [TestClass()]
     public class ProgramTests
     {
         [TestMethod()]
         public void fTest()
         {
-            
+            //Assert.AreEqual(
+            //    new A { i = 1 },
+            //    new A { i = 1 }
+            //    );    //падает
+
+            CollectionAssert.AreEqual(
+                new[] { new A { i = 1 } },
+                new[] { new A { i = 1 } },
+                new MyComparer()
+                );
+
+
         }
     }
 }
