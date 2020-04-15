e61085d1366a74572d2595afb4f60b584abbf7bf
Aliq <Alexandr.V.Kim@kaspi.kz>
2020-04-15 18:43:21 +0600

    при резолве указываем, что некий интерфейс нужно резолвить резолвом другого ~класса

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Unity;
+using Unity.Injection;
+using Unity.Resolution;
 
 namespace ConsoleApp1
 {
@@ -13,14 +15,61 @@ namespace ConsoleApp1
         {
             using (var container = new UnityContainer())
             {
-                var a = container.Resolve<A>();
+                container.EnableDebugDiagnostic();
 
+                container.RegisterType<I, C1>();
+
+                container.Resolve<A>().f();
+                container.Resolve<A>(
+                    new DependencyOverride<I>(
+                        new ResolvedParameter(typeof(C2))))
+                .f();
             }
+            //https://docs.microsoft.com/en-us/previous-versions/msp-n-p/ff660882%28v%3dpandp.20%29
+            //https://docs.microsoft.com/en-us/previous-versions/msp-n-p/ff660920(v=pandp.20)?redirectedfrom=MSDN
 
             Console.WriteLine("DONE");
             Console.ReadKey();
         }
 
-        class A { };
+        class A {
+            private readonly B b;
+
+            public A(B b)
+            {
+                this.b = b;
+            }
+
+            public void f()
+            {
+                b.c.SayMyName();
+            }
+        };
+        class B {
+            public I c;
+
+            public B(I c)
+            {
+                this.c = c;
+            }
+        };
+        interface I
+        {
+            void SayMyName();
+        };
+        class C1 : I
+        {
+            public void SayMyName()
+            {
+                Console.WriteLine("C1");
+            }
+        }
+        class C2 : I
+        {
+            public void SayMyName()
+            {
+                Console.WriteLine("C2");
+            }
+        }
     }
 }
