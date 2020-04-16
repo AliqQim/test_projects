d0f90091015b19d7a3fc1decdf4cdd47f4889258
Aliq <Alexandr.V.Kim@kaspi.kz>
2020-04-16 18:31:31 +0600

    демонстрирует, что в стектрейс исключения кладутся только методы до того места, где исключение поймано

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,10 +10,46 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            try
+            {
+                a();
 
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"***catch in Main():\n{e.StackTrace}\n==============");
+            }
+    
 
             Console.WriteLine("DONE");
             Console.ReadKey();
+        }
+
+
+        static void a() { b(); }
+
+        private static void b()
+        {
+            c();
+        }
+
+        private static void c()
+        {
+            try
+            {
+                d();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"***catch in c():\n{e.StackTrace}\n==============");
+                throw;
+            }
+            
+        }
+
+        private static void d()
+        {
+            throw new NotImplementedException();
         }
     }
 }
