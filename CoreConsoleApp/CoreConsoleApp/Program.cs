b0b0c3a44f636bc4aecd848a7d7aafd48b4c5773
AliqQim <hnero@mail.ru>
2020-05-22 20:50:54 +0600

    перенес DbContext, но еще не создаю

--- CoreConsoleApp/CoreConsoleApp/Program.cs
+++ CoreConsoleApp/CoreConsoleApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace CoreConsoleApp
 {
@@ -6,7 +8,34 @@ namespace CoreConsoleApp
     {
         static void Main(string[] args)
         {
+            //var options = new new DbContextOptionsBuilder<MyContext>()
+            //    .UseSql
+
             Console.WriteLine("DONE");
         }
+    }
+
+    public class Person
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+        public int Age { get; set; }
+
+
+
+    }
+
+
+
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Person> Persons { get; set; } = null!;
     }
 }
