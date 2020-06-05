415bc2146dcb9a3c7ecf012ebd17002fdb6db4ec
AliqQim <hnero@mail.ru>
2020-06-05 20:52:32 +0600

    добавил метод с параметром

--- WebAPI.Core/WebAPI.Core/Controllers/WeatherForecastController.cs
+++ WebAPI.Core/WebAPI.Core/Controllers/WeatherForecastController.cs
@@ -35,5 +35,12 @@ namespace WebAPI.Core.Controllers
             })
             .ToArray();
         }
+
+
+        [HttpPost]
+        public double GetAverageTemperatures(double value1, double value2)
+        {
+            return (value1 + value2) / 2;
+        }
     }
 }
