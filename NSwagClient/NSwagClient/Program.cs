f57ce9d61dfed5eefab5682ffc779f524b62d13d
AliqQim <hnero@mail.ru>
2020-06-10 22:03:05 +0600

    первое обращение сработало

--- NSwagClient/NSwagClient/Program.cs
+++ NSwagClient/NSwagClient/Program.cs
@@ -1,11 +1,22 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using test_proxies;
 
 namespace NSwagClient
 {
     class Program
     {
-        static void Main(string[] args)
+        async static Task Main(string[] args)
         {
+            using var httpClient = new HttpClient();
+
+            var client = new swaggerClient("https://localhost:44354/", httpClient);
+
+            Console.WriteLine("обращаемся к сервису...");
+            Console.WriteLine(await client.WeatherForecastAsync(1, 2));
+
+
             Console.WriteLine("Hello World!");
         }
     }
