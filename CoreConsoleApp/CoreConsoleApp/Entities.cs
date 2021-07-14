a17da5f61f36600e062181b2e4d779d3c65066b7
alik <alexandrkim@tengizchevroil.com>
2021-07-14 17:43:30 +0600

    get rid of nullability (it brings additional complexity , but I need these tests for a project with nullability turned off)

--- CoreConsoleApp/CoreConsoleApp/Entities.cs
+++ CoreConsoleApp/CoreConsoleApp/Entities.cs
@@ -10,38 +10,38 @@ namespace CoreConsoleApp
     {
         public int Id { get; set; }
 
-        public string Name { get; set; } = null!;
+        public string Name { get; set; }
         public int Age { get; set; }
 
-        public List<Zamorochka> OwnZamorochkas { get; set; } = null!;
+        public List<Zamorochka> OwnZamorochkas { get; set; }
 
-        public Zamorochka UnforgivableZamorochkaOfOtherPerson{ get; set; } = null!;
+        public Zamorochka UnforgivableZamorochkaOfOtherPerson{ get; set; }
 
-        public Job Job { get; set; } = null!;
+        public Job Job { get; set; }
 
     }
 
     public class Zamorochka
     {
-        public string Name { get; set; } = null!;
+        public string Name { get; set; }
         public int ZodiacId { get; set; }
-        public Zodiac Zodiac { get; set; } = null!;
+        public Zodiac Zodiac { get; set; }
 
-        public override string? ToString() => $"{Name} ({Zodiac?.Name})";
+        public override string ToString() => $"{Name} ({Zodiac?.Name})";
     }
 
     public class Job
     {
         [Key]
         public int Id { get; set; }
-        public string Name { get; set; } = null!;
+        public string Name { get; set; }
     }
 
     public class Zodiac
     {
         [Key]
         public int Id { get; set; }
-        public string Name { get; set; } = null!;
+        public string Name { get; set; }
     }
 
     public class MyContext : DbContext
@@ -51,8 +51,8 @@ namespace CoreConsoleApp
         {
         }
 
-        public virtual DbSet<Person> Persons { get; set; } = null!;
-        public virtual DbSet<Zodiac> Zodiacs { get; set; } = null!;
+        public virtual DbSet<Person> Persons { get; set; }
+        public virtual DbSet<Zodiac> Zodiacs { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
