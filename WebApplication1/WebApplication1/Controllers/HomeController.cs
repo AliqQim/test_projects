a075e7377c9467d80d805ab3581c0f01ed638ab0
Alexandr.Kim <alexandr.kim@arcadia.spb.ru>
2021-11-19 23:42:16 +0600

    soap call works

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.ServiceModel;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ServiceReference1;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
@@ -18,9 +20,29 @@ namespace WebApplication1.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var one = await GetNumberText(1);
+
+            ViewBag.RequestResult = one;
             return View();
+        }
+
+        private static async Task<string> GetNumberText(ulong num)
+        {
+            var httpBinding = new BasicHttpBinding(BasicHttpSecurityMode.Transport);
+            var address = new EndpointAddress(@"https://www.dataaccess.com/webservicesserver/NumberConversion.wso");
+
+            var factory = new ChannelFactory<NumberConversionSoapType>(httpBinding, address);
+
+            var request = new NumberToWordsRequest
+            {
+                Body = new NumberToWordsRequestBody
+                {
+                    ubiNum = num
+                }
+            };
+            return (await factory.CreateChannel().NumberToWordsAsync(request)).Body.NumberToWordsResult;
         }
 
         public IActionResult Privacy()
