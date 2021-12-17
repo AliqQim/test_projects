1f464c183353f7c62a426aa0b7eb222fa6e95014
Alexandr.Kim <alexandr.kim@arcadia.spb.ru>
2021-12-17 17:09:27 +0600

    values bound

--- knockout_site/Script.js
+++ knockout_site/Script.js
@@ -1,16 +1,14 @@
 ﻿// This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
 function AppViewModel() {
-    this.firstName = ko.observable("Bert");
-    this.lastName = ko.observable("Bertington");
+    this.strings = ko.observableArray([
+        { str: ko.observable("qwe")},
+        { str: ko.observable("rty")}
+    ]);
 
-    this.fullName = ko.computed(function() {
-        return this.firstName() + " " + this.lastName();    
-    }, this);
-
-    this.capitalizeLastName = function() {
-        var currentVal = this.lastName();        // Read the current value
-        this.lastName(currentVal.toUpperCase()); // Write back a modified value
-    };    
+    this.showSmth=() => {
+        alert(999); //**!!
+    }
+      
 }
 
 // Activates knockout.js
