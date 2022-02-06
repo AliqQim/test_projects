aeeb56690f39b43a530de73957b4a90722482013
Alexandr.Kim <alexandr.kim@arcadia.spb.ru>
2022-02-06 17:02:26 +0600

    folder mounted

--- docker_linux/run.bat
+++ docker_linux/run.bat
@@ -1,6 +1,7 @@
+
 docker build . -t mylinux_image
 
-docker run --volume /extenral_folder:../attached_folder --rm -it --name=mylinux mylinux_image
+docker run --volume %~dp0..\attached_folder:/external_folder  --rm -it --name=mylinux mylinux_image
 
 docker attach mylinux 
 
@@ -8,3 +9,4 @@ docker attach mylinux
 @rem --rm - контейнер удаляется после выключения
 @rem -it - контейнер после запуска типа остается в режиме ожидания. 
 @rem -d - контейнер во время выполнения (или режима ожидания) не занимает текущую консоль
+@rem %~dp0 - путь к текущей папке
