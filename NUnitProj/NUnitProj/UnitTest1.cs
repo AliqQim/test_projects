a11507d35d0164b0da7ee71ad2a9817e02d880ae
Alexandr.Kim <alexandr.kim@arcadia.spb.ru>
2022-03-14 22:47:47 +0600

    Test initialize

--- NUnitProj/NUnitProj/UnitTest1.cs
+++ NUnitProj/NUnitProj/UnitTest1.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System.Diagnostics;
 
 namespace NUnitProj
 {
@@ -7,12 +8,25 @@ namespace NUnitProj
         [SetUp]
         public void Setup()
         {
+            Debug.WriteLine("***************Setup");
+        }
+        
+        [TearDown]
+        public void TearDown()
+        {
+            Debug.WriteLine("***************TearDown");
         }
 
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            Debug.WriteLine("***************Test1");
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Debug.WriteLine("***************Test2");
         }
     }
 }
