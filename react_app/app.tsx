ea72927a23fdf43404713c0202f199da4b9c6372
Alexandr.Kim <alexandr.kim@arcadia.spb.ru>
2022-03-17 23:11:42 +0600

    ready

--- react_app/app.tsx
+++ react_app/app.tsx
@@ -1,16 +1,23 @@
 import * as React from 'react';
 import axios from "axios";
-function  App() {
-    
-    axios({
 
-        // Endpoint to send files
-        url: "https://localhost:44386/weatherforecast",
-        method: "GET",
-    }).then(data => { debugger });  //**!!
+let loaded = false;
+function  App() {
+    const [value, setValue] = React.useState("Loading...");
+
+    if (!loaded) {
+        axios({
+
+            // Endpoint to send files
+            url: "https://localhost:44386/weatherforecast",
+            method: "GET",
+        }).then(data => setValue(JSON.stringify(data, null, 4)));
+        loaded = true;
+    }
+    
 
     return (
-		<div>{'react setup from scratch without cra testing ts'}</div>
+		<pre>{value}</pre>
 	);
 }
 export default App;
