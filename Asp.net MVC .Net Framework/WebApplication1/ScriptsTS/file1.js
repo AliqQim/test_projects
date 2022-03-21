606d0c5017a00540d89754f3fdb3f10ca3c7437c
Alexandr.Kim <alexandr.kim@arcadia.spb.ru>
2022-03-21 16:14:20 +0600

    Promise events sequence

--- Asp.net MVC .Net Framework/WebApplication1/ScriptsTS/file1.js
+++ Asp.net MVC .Net Framework/WebApplication1/ScriptsTS/file1.js
@@ -1,2 +1,31 @@
-alert(999);
-//# sourceMappingURL=file1.js.map
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+let promiseResolve = null;
+function promiser() {
+    console.log("before creating promise");
+    let res = new Promise((resolve) => {
+        console.log("inside the promise callback");
+        promiseResolve = resolve;
+    });
+    console.log("after creating promise");
+    return res;
+}
+function f() {
+    return __awaiter(this, void 0, void 0, function* () {
+        setTimeout(() => doResolve(), 1000);
+        console.log("before awaiting");
+        var value = yield promiser();
+        console.log("awaited value is:" + value);
+    });
+}
+function doResolve() {
+    promiseResolve("the promised string value");
+}
+f();
--- Asp.net MVC .Net Framework/WebApplication1/ScriptsTS/file1.ts
+++ Asp.net MVC .Net Framework/WebApplication1/ScriptsTS/file1.ts
@@ -1,1 +1,28 @@
-﻿alert(999)
+﻿
+
+let promiseResolve = null;
+
+function promiser(): Promise<string> {
+    console.log("before creating promise");
+    let res = new Promise<string>((resolve) => {
+        console.log("inside the promise callback");
+        promiseResolve = resolve;
+    });
+    console.log("after creating promise");
+    return res;
+}
+
+async function f() {
+    setTimeout(()=>doResolve(), 1000);
+
+    console.log("before awaiting");
+    var value = await promiser();
+    console.log("awaited value is:" + value);
+
+}
+
+function doResolve() {
+    promiseResolve("the promised string value");
+}
+
+f();
