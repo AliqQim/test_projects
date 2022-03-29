5eadcd9f46f22a512ac98580876be0e87a34648d
Alexandr.Kim <alexandr.kim@arcadia.spb.ru>
2022-03-29 12:00:36 +0600

    test of a code is ready

--- Asp.net MVC .Net Framework/WebApplication1/mocha/my_test.js
+++ Asp.net MVC .Net Framework/WebApplication1/mocha/my_test.js
@@ -1,8 +1,8 @@
 let assert = chai.assert;
-describe('Array', function () {
-    describe('#indexOf()', function () {
-        it('should return -1 when the value is not present', function () {
-            assert.equal([1, 2, 3].indexOf(4), -1);
-        });
+describe('#Adder', function () {
+    it('Add should return correctly sum', () => {
+        let target = new Modiule1.Adder();
+        let res = target.add(1, 1);
+        assert.equal(res, 2);
     });
 });
