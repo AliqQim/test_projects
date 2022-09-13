bf864067141b50ac620880a9e35d68ee2a84a0b4
AliqQim <alexandr.kim@sanoma.com>
2022-09-13 19:31:54 +0600

    no-nullability-project-reference

---
+++ CoreConsoleApp/ClassLibraryNoNullability/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassLibraryNoNullability
+{
+    public class Class1
+    {
+        public static Class1 CreateInstanceReturnsNull()
+            => null;    //no warning
+
+        public void SayHi() => Console.WriteLine("hi!");
+    }
+}
--- CoreConsoleApp/CoreConsoleApp/Program.cs
+++ CoreConsoleApp/CoreConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassLibraryNoNullability;
+using System;
 
 namespace CoreConsoleApp
 {
@@ -6,7 +7,16 @@ namespace CoreConsoleApp
     {
         static void Main(string[] args)
         {
+            Class1 c = Class1.CreateInstanceReturnsNull();  //I expected a warning here, but there is no 
+            c.SayHi();
+
             Console.WriteLine("DONE");
+        }
+
+        public static Program NullabilityCheck()
+        {
+            //return null;    //warning
+            throw new NotSupportedException();
         }
     }
 }
