feb00fd2be666b707c01a6ecc5441cb2f5e6c82a
AliqQim <alexandr.kim@sanoma.com>
2022-09-14 21:47:56 +0600

    nullable varb treated ass non nullable

--- CoreConsoleApp/CoreConsoleApp/Program.cs
+++ CoreConsoleApp/CoreConsoleApp/Program.cs
@@ -7,8 +7,12 @@ namespace CoreConsoleApp
     {
         static void Main(string[] args)
         {
-            Class1 c = Class1.CreateInstanceReturnsNull();  //I expected a warning here, but there is no 
-            c.SayHi();
+            Class1 c = Class1.CreateInstanceReturnsNull();  //I expected a warning here, but there is no one
+            c.SayHi();  //NullReferenceException
+
+            Program? p = new Program();
+            p.SayHiFromProgram();   //no warning althought p is of nullable type.
+                                    //becasuse compiler sees that it definitey intialized with an object
 
             Console.WriteLine("DONE");
         }
@@ -18,5 +22,7 @@ namespace CoreConsoleApp
             //return null;    //warning
             throw new NotSupportedException();
         }
+
+        public void SayHiFromProgram() => Console.WriteLine("hi from program!");
     }
 }
