ecfc0cd7f18a006fdcfa36902d667a0e229fddfb
AliqQim <alexandr.kim@sanoma.com>
2022-12-08 19:02:21 +0600

    FileStream_StreamReader_StreamWriter

--- CoreConsoleApp/CoreConsoleApp/Program.cs
+++ CoreConsoleApp/CoreConsoleApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 namespace CoreConsoleApp
 {
@@ -6,6 +8,39 @@ namespace CoreConsoleApp
     {
         static void Main(string[] args)
         {
+            {
+                using var stream = new FileStream(@"c:\tmp\test_output.txt", FileMode.CreateNew);
+                using var writer = new StreamWriter(stream, Encoding.UTF8);
+                writer.Write("Алик ел салат");
+            }
+
+            {
+                using var stream = new FileStream(@"c:\tmp\test_output2.txt", FileMode.CreateNew);
+                using var writer = new StreamWriter(stream, Encoding.UTF8);
+                writer.Write(" и закусывал простоквашей");
+            }
+
+            {
+                using var stream = new FileStream(@"c:\tmp\test_output3.txt", FileMode.CreateNew);
+                using var writer = new StreamWriter(stream, Encoding.UTF8);
+
+                using var src1 = new FileStream(@"c:\tmp\test_output.txt", FileMode.Open, FileAccess.Read);
+                using var reader1 = new StreamReader(src1);
+                using var src2 = new FileStream(@"c:\tmp\test_output2.txt", FileMode.Open, FileAccess.Read);
+                using var reader2 = new StreamReader(src2);
+
+
+                while (!reader1.EndOfStream)
+                    writer.Write((char)reader1.Read());
+
+                while (!reader2.EndOfStream)
+                    writer.Write((char)reader2.Read());
+
+
+
+            }
+
+
             Console.WriteLine("DONE");
         }
     }
