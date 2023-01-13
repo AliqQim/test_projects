68327e77726128c726707c7abc52c91c902912e4
AliqQim <alexandr.kim@sanoma.com>
2023-01-13 23:34:19 +0600

    simple constructing

--- CoreConsoleApp/CoreConsoleApp/Program.cs
+++ CoreConsoleApp/CoreConsoleApp/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Threading.Channels;
+using StrongInject;
 
 namespace CoreConsoleApp
 {
@@ -6,7 +9,43 @@ namespace CoreConsoleApp
     {
         static void Main(string[] args)
         {
+            new Container().Run(x => x.f());
             Console.WriteLine("DONE");
         }
+
     }
+
+    [Register(typeof(B), typeof(IB))]
+    [Register(typeof(A), typeof(IA))]
+    public partial class Container: IContainer<IB>{}
+
+    public interface IA
+    {
+
+    }
+
+    public class A : IA
+    {
+
+    }
+
+    public interface IB
+    {
+        void f();
+    }
+
+    public class B : IB
+    {
+        private readonly IA _a;
+
+        public B(IA a)
+        {
+            _a = a;
+            
+        }
+
+        public void f()=> Console.WriteLine(_a);
+
+    }
+
 }
