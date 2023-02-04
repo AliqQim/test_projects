32a2626afbf492843c9143d7baa62e2fbd748f1b
AliqQim <alexandr.kim@sanoma.com>
2023-02-04 20:07:36 +0600

    autofac-per-scope-lifetime-vs-singleton

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -13,25 +13,65 @@ namespace ConsoleApp1
         {
 
             var builder = new ContainerBuilder();
+            builder.RegisterType<Logger>().AsSelf().SingleInstance();
+            builder.RegisterType<A>().AsSelf().InstancePerLifetimeScope();
+            builder.RegisterType<B>().AsSelf().InstancePerLifetimeScope();
+            builder.RegisterType<C>().AsSelf().InstancePerLifetimeScope();
 
-            builder.RegisterModule<TestModule>();
-            builder.RegisterModule<TestModule>();
+            A a1, a2;
+            Logger logger;
 
-            var container = builder.Build();
+            using (var container = builder.Build())
+            {
+                using (var outerScope = container.BeginLifetimeScope())
+                {
+                    logger = outerScope.Resolve<Logger>();
 
-            container.Resolve<I>().Greet();
+                    using (var innerScope1 = outerScope.BeginLifetimeScope())
+                    {
+                        a1 = innerScope1.Resolve<A>();
+                    }
+                    using (var innerScope2 = outerScope.BeginLifetimeScope())
+                    {
+                        a2 = innerScope2.Resolve<A>();
+                    }
+                }
+
+            }
+            //ensure by means of debugger that logger is same instance everywhere,
+            //and each A instance have their own instance of b. and C has same B instance as A.
 
             Console.WriteLine("DONE");
             Console.ReadKey();
         }
     }
-    public interface I
+
+    public class Logger
     {
-        void Greet();
+        
     }
-    public class A: I
-    {
-        public void Greet() => Console.WriteLine("Hi!");
+
+    public class A {
+        private readonly Logger logger;
+        private readonly B b;
+        private readonly C c;
+
+        public A(Logger logger, B b, C c)
+        {
+            this.logger = logger;
+            this.b = b;
+            this.c = c;
+        }
+    } 
+    public class B { } 
+    public class C {
+        private readonly B b;
+
+        public C(B b)
+        {
+            this.b = b;
+        }
     }
+
 
 }
