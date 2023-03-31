3b4807129ee5808134bb7b70570bb92ad881d043
AliqQim <alexandr.kim@sanoma.com>
2023-03-31 21:34:37 +0600

    added Standard class library

---
+++ ConsoleApp1/ClassLibrary2/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ClassLibrary2
+{
+    public class Class1
+    {
+        public static void CoreF() => Console.WriteLine("Hi from Core!");
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,4 @@
+using ClassLibrary2;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +11,45 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            //new Container().Run(x => x.f());
 
+            Class1.CoreF();
 
             Console.WriteLine("DONE");
             Console.ReadKey();
         }
     }
+
+    //[Register(typeof(B), typeof(IB))]
+    //[Register(typeof(A), typeof(IA))]
+    //public partial class Container : IContainer<IB> { }
+
+    //public interface IA
+    //{
+
+    //}
+
+    //public class A : IA
+    //{
+
+    //}
+
+    //public interface IB
+    //{
+    //    void f();
+    //}
+
+    //public class B : IB
+    //{
+    //    private readonly IA _a;
+
+    //    public B(IA a)
+    //    {
+    //        _a = a;
+
+    //    }
+
+    //    public void f() => Console.WriteLine(_a);
+
+    //}
 }
