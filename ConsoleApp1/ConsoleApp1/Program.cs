58643d90cc4f418874da6dcf71d74e1a73de1ba5
AliqQim <alexandr.kim@sanoma.com>
2023-12-01 19:46:57 +0600

    log4net: target loggersettings are chosen by the beginning of requested logger name

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,5 @@
+using log4net;
+using log4net.Config;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +13,14 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
+            var config = new System.IO.FileInfo("MyConfigFolder/mylog4netconfig.xml");
+            XmlConfigurator.Configure(config);
+
+            var logAaa = LogManager.GetLogger("aaa.ccc");
+            logAaa.Info("log string - aaa ");  //should be written to aaa-logger
+
+            var logBbb= LogManager.GetLogger("log string - bbb");
+            logBbb.Info("bbb"); //should be written to the root-logger
 
             Console.WriteLine("DONE");
             Console.ReadKey();
