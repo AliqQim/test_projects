65cc687b72b254f764208e310f7903abf28e6866
AliqQim <alexandr.kim@sanoma.com>
2024-04-26 13:51:53 +0500

    classes arranged

--- CoreConsoleApp/CoreConsoleApp/Program.cs
+++ CoreConsoleApp/CoreConsoleApp/Program.cs
@@ -1,3 +1,29 @@
 ﻿
+var a = new A { Fld1 = 222, LinkToCObj = new C1 { SomeFld = "Uraa!" } };
+
 
 Console.WriteLine("Hi!");
+
+public class A
+{
+    public int Fld1 { get; set; }
+    public C1? LinkToCObj { get; set; }
+
+}
+
+public class B
+{
+    public int Fld1 { get; set; }
+    public C2? LinkToCObj { get; set; }
+
+}
+
+public class C1
+{
+    public string? SomeFld { get; set; }
+}
+
+public class C2
+{
+    public string? SomeFld { get; set; }
+}
