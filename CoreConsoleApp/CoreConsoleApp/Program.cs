e1f41d60c9dc7cfefe37b3ea4e847e5a8229b1c0
AliqQim <alexandr.kim@sanoma.com>
2024-04-26 14:04:31 +0500

    exception where it should be

--- CoreConsoleApp/CoreConsoleApp/Program.cs
+++ CoreConsoleApp/CoreConsoleApp/Program.cs
@@ -1,8 +1,26 @@
 ﻿
+using AutoMapper;
+
 var a = new A { Fld1 = 222, LinkToCObj = new C1 { SomeFld = "Uraa!" } };
 
 
+var config = new MapperConfiguration(cfg => cfg.AddProfile(new MappingProfile()));
+var mapper = config.CreateMapper();
+
+
+var b = mapper.Map<B>(a); //exception here because there is no C1 -> C2 mapping
+
+
 Console.WriteLine("Hi!");
+
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<A, B>();
+    }
+}
 
 public class A
 {
