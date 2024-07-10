a6cd58a632a43b9a38a376c4c19b8fa07dd12586
AliqQim <alexandr.kim@sanoma.com>
2024-07-10 23:09:00 +0500

    service is nicely called

--- CoreConsoleApp/SoapClient/Program.cs
+++ CoreConsoleApp/SoapClient/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using ServiceReference1;
+
+await using var client = new ProfileServiceClient();
+var profile = await client.GetProfileAsync(1);
+
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
