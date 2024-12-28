8668527c7370f1c70ed20899ed4fd48b270b5570
AliqQim <qim.aliq@gmail.com>
2024-12-28 20:15:41 +0500

    rethrown exception behaves same way as usual one - displays full stacktrace

--- CoreConsoleApp/CoreConsoleApp/Program.cs
+++ CoreConsoleApp/CoreConsoleApp/Program.cs
@@ -1,3 +1,31 @@
 ﻿
+try
+{
+    f2();
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+    Console.WriteLine(e.StackTrace);
+}
 
-Console.WriteLine("Hi!");
+
+
+void f1()
+{
+    throw new Exception("Ura!");
+}
+
+void f2()
+{
+    try
+    {
+        f1();
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+        Console.WriteLine(e.StackTrace);
+        throw;
+    }
+}
