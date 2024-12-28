935cdbbb0b537efc449ac4e83efa35074ac94804
AliqQim <qim.aliq@gmail.com>
2024-12-28 20:23:43 +0500

    rethrown exception contains line of re-throwing but not line inside the method

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,10 +10,37 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-
+            try
+            {
+                f2();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
 
             Console.WriteLine("DONE");
             Console.ReadKey();
+        }
+
+        static void f1()
+        {
+            throw new Exception("Ura!");
+        }
+
+        static void f2()
+        {
+            try
+            {
+                f1();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+                throw;
+            }
         }
     }
 }
