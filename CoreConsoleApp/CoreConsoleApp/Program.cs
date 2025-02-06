6ab7e67cf5a876465e75890ac1fb8de38994e08a
AliqQim <qim.aliq@gmail.com>
2025-02-06 23:05:32 +0500

    a key is bound to another prop in successor class

--- CoreConsoleApp/CoreConsoleApp/Program.cs
+++ CoreConsoleApp/CoreConsoleApp/Program.cs
@@ -1,6 +1,38 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json;
 
-Console.WriteLine(JObject.Parse("{}"));
+//.Net Framework [ same behaviour
+
+string json = @"{ ""Name"": ""John Doe"", ""Age"": 30 }";
+Class1 obj = JsonConvert.DeserializeObject<Class1>(json);
+Class2 obj2 = JsonConvert.DeserializeObject<Class2>(json);
+
+Console.WriteLine($"Name: {obj.Name}, Age: {obj.Age}");
 
 Console.WriteLine("DONE");
 Console.ReadKey();
+
+
+class Class1
+{
+    [JsonProperty("Name")]
+    public virtual string? Name { get; set; }
+
+    [JsonProperty("Age")]
+    public int Age { get; set; }
+
+
+}
+
+class Class2 : Class1
+{
+    [JsonIgnore]
+    public override string? Name
+    {
+        get => $"Hoi: {NameRaw}";
+        set => throw new NotImplementedException();
+    }
+
+    [JsonProperty("Name")]
+    public virtual string? NameRaw { get; set; }
+}
+
