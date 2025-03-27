b1f020dfe18db7763f473c1ee75ba39872c00848
AliqQim <qim.aliq@gmail.com>
2025-03-27 17:41:41 +0500

    ICollection - casts to a colelction of a base type througt ToList (new list generation)

--- CoreConsoleApp/CoreConsoleApp/Program.cs
+++ CoreConsoleApp/CoreConsoleApp/Program.cs
@@ -1,3 +1,36 @@
 ﻿
+var b = new B<BItem>();
+
+var res = b.GetCollection();
+
 
 Console.WriteLine("Hi!");
+
+
+class Item
+{
+}
+
+interface IA
+{
+
+
+    ICollection<Item> GetCollection();
+}
+
+
+class BItem : Item
+{
+}
+
+class B<TItem> : IA where TItem: new()
+{
+    public ICollection<Item> GetCollection()
+    {
+        var res = new List<TItem>{new TItem()};
+
+        return ((IEnumerable<Item>)res).ToList();
+
+        
+    }
+}
